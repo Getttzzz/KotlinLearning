43a012177bc9cb72668ce5094cb4c18c2d0b6a44
getz <getttzzz@gmail.com>
2018-09-26 02:30:44 +0300

    Add empty tests.

--- app/src/test/java/com/example/user/kotlinlearning/ExampleUnitTest.kt
+++ app/src/test/java/com/example/user/kotlinlearning/ExampleUnitTest.kt
@@ -1,22 +1,20 @@
 package com.example.user.kotlinlearning
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testFilter() {
 
 
-
+        assertEquals(1, 1)
     }
 
+    @Test
+    fun testMap(){
 
+
+        assertEquals(1,1)
+    }
 }
